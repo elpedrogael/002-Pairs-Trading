05331d3668c46059ffdbc19ad26beff09fe35174
elpedrogael <pedro.rayas@iteso.mx>
2025-02-27 20:33:13 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import pandas as pd
 from kalman_filter import KalmanFilter
 from cointegration import check_cointegration, calculate_hedge_ratio
 from backtest import backtest_strategy
+from data_process import process_data
+
 
 def main():
-    data = pd.read_csv('data/historical_data.csv')
+    data = process_data()
+
     if check_cointegration(data):
-        kf = KalmanFilter()
-        # Implement trading logic and backtesting
+        hedge_ratios = calculate_hedge_ratio(data['Close'], data['Close.1'])
+        backtest_strategy(data, hedge_ratios.mean())
     else:
         print("The selected assets are not cointegrated.")
+
 
 if __name__ == "__main__":
     main()
