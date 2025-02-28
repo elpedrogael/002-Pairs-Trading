beb209e035ff00ca5e55d6597853f324a1382f8a
Evelin Ramirez <112009580+evelinrq@users.noreply.github.com>
2025-02-27 19:37:13 -0600

    Add files via upload

--- data_process.py
+++ data_process.py
@@ -1,29 +1,14 @@
 import pandas as pd
 
-file_path = "data/historical_data.csv"
-data = pd.read_csv(file_path)
+def process_data(file_path= "data/historical_data.csv"):
+    data = pd.read_csv("data/historical_data.csv")
+    data_cleaned = data.iloc[1:].reset_index(drop=True)
+    data_cleaned["Date"] = pd.to_datetime(data_cleaned["Date"])
+    numeric_columns = ['Close', 'Close.1', 'High', 'High.1', 'Low', 'Low.1', 'Open', 'Open.1', 'Volume', 'Volume.1']
+    data_cleaned[numeric_columns] = data_cleaned[numeric_columns].astype(float)
+    return data_cleaned
 
-data_info = data.info()
-data_head = data.head()
-
-missing_values = data.isnull().sum()
-
-columns = data.columns
-
-data_info, data_head, missing_values, columns
-
-#
-
-data_cleaned = data.iloc[2:].reset_index(drop=True)
-
-numeric_columns = ['Close', 'Close.1', 'High', 'High.1', 'Low', 'Low.1', 'Open', 'Open.1', 'Volume', 'Volume.1']
-data_cleaned[numeric_columns] = data_cleaned[numeric_columns].astype(float)
-
-data_cleaned = data_cleaned.dropna()
-
-data_cleaned.head()
-
-data_cleaned = data_cleaned.rename(columns={'Price': 'Date'})
-data_cleaned['Date'] = pd.to_datetime(data_cleaned['Date'])
-
-data_cleaned.head()
+if __name__ == "__main__":
+    file_path = "data/historical_data.csv"
+    data_cleaned = process_data(file_path)
+    data_cleaned.to_csv('data/processed_data.csv', index=False)
